be5dbe8a02b092b9cce7f9911cc071ff6df39df3
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-07 16:22:33 +0100

    add collapsed

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const designLegend = document.querySelector('.js-design-legend');
+
+const fillLegend = document.querySelector('.js-fill-legend');
+
+const shareLegend = document.querySelector('.js-share-legend');
+
+const designForm = document.querySelector('.js-design-form');
+
+const fillForm = document.querySelector('.js-fill-form');
+
+const shareForm = document.querySelector('.js-share-form');
