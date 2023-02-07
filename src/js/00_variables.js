bfb9c88ac326647e6012b35393ad67d36d260a22
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-07 17:20:26 +0100

    add function

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -11,3 +11,9 @@ const designForm = document.querySelector('.js-design-form');
 const fillForm = document.querySelector('.js-fill-form');
 
 const shareForm = document.querySelector('.js-share-form');
+
+const shareArrow = document.querySelector('.js-share-arrow');
+
+const fillArrow = document.querySelector('.js-fill-arrow');
+
+const designArrow = document.querySelector('.js-design-arrow');
