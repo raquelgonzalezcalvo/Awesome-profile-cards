b891ceee08ade2e5e828e7eae9862642c9989528
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-02-08 13:09:56 +0100

    review functions collapsed

--- src/js/01_colapsables.js
+++ src/js/01_colapsables.js
@@ -1,4 +1,6 @@
 /* eslint-disable strict */
+
+/*
 fillLegend.addEventListener('click', handleClickFill);
 
 function handleClickFill (event) {
@@ -58,3 +60,53 @@ function handleClickDesign (event) {
     }
    
 }
+*/
+
+function closeAllSections() {
+    designForm.classList.add('collapsed');
+    fillForm.classList.add('collapsed');
+    shareForm.classList.add('collapsed');
+}
+
+function turnDownAllArrows() {
+    designArrow.classList.add('arrow-collapsed');
+    fillArrow.classList.add('arrow-collapsed');
+    shareArrow.classList.add('arrow-collapsed');
+}
+
+function openAndTurnDesignSection() {
+    designForm.classList.remove('collapsed');
+    designArrow.classList.remove('arrow-collapsed');
+}
+
+function handleClickDesign(event) {
+    closeAllSections();
+    turnDownAllArrows();
+    openAndTurnDesignSection();
+}
+
+function openAndTurnFillSection() {
+    fillForm.classList.remove('collapsed');
+    fillArrow.classList.remove('arrow-collapsed');
+}
+
+function handleClickFill(event) {
+    closeAllSections();
+    turnDownAllArrows();
+    openAndTurnFillSection();
+}
+
+function openAndTurnShareSection() {
+    shareForm.classList.remove('collapsed');
+    shareArrow.classList.remove('arrow-collapsed');
+}
+
+function handleClickShare(event) {
+    closeAllSections();
+    turnDownAllArrows();
+    openAndTurnShareSection();
+}
+
+designLegend.addEventListener('click', handleClickDesign);
+fillLegend.addEventListener('click', handleClickFill);
+shareLegend.addEventListener('click', handleClickShare);
