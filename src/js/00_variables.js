ac6b5aa039c61dea36d0c3456aba608156c1b593
manuelainclan <minclanfigueiras@gmail.com>
2023-02-08 18:44:53 +0100

    Add palettes js

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -18,6 +18,9 @@ const fillArrow = document.querySelector('.js-fill-arrow');
 
 const designArrow = document.querySelector('.js-design-arrow');
 
+
+// ----------------------------------PALETTE -----------------------------------------
+
 const buttonPalette1 = document.querySelector ('.js-buttonpalette1');
 
 const buttonPalette2 = document.querySelector ('.js-buttonpalette2');
@@ -26,9 +29,33 @@ const buttonPalette3 = document.querySelector ('.js-buttonpalette3');
 
 const elementsPalette3 = document.getElementsByClassName ('palette3');
 
+const previewRectangle = document.querySelector ('.js-preview-rectangle');
+
+const borderMobile = document.querySelector ('.js-border-mobile');
+
+const borderEmail = document.querySelector ('.js-border-e-mail');
+
+const borderLinkedin = document.querySelector ('.js-border-linkedin');
+
+const borderGithub = document.querySelector ('.js-border-github');
+
+
+// --------------------------------RELLENO TARJETA------------------------------------
+
 const inputName = document.querySelector('.js-fullName');
 const previewName = document.querySelector('.js-name');
 
 const inputJob = document.querySelector('.js-job');
 const previewJob = document.querySelector('.js-preview-job');
 
+const inputMail = document.querySelector('.js-email');
+const previewMail = document.querySelector('.js-icon-e-mail');
+
+const inputPhone = document.querySelector('.js-phone');
+const previewPhone = document.querySelector('.js-icon-mobile');
+
+const inputLinkedin = document.querySelector('.js-linkedin');
+const previewLinkedin = document.querySelector('.js-icon-linkedin');
+
+const inputGithub = document.querySelector('.js-github');
+const previewGithub = document.querySelector('.js-icon-github');
--- src/js/02_preview.js
+++ src/js/02_preview.js
@@ -1,4 +1,6 @@
 /* eslint-disable strict */
+
+// PREGUNTAR
 // function handleClickPalette2 (event) {
 //     event.preventDefault ();
 //     console.log ('holisss');
@@ -16,3 +18,122 @@
 
 // buttonPalette2.addEventListener('click', handleClickPalette2);
 
+// Meter iconos, hacer cambie color bien, funciones para hacerlo más simple
+
+
+function deletePalette2() {
+previewRectangle.classList.remove('palette2');
+borderMobile.classList.remove('palette2'); 
+borderEmail.classList.remove('palette2'); 
+borderLinkedin.classList.remove('palette2'); 
+borderGithub.classList.remove('palette2');
+previewName.classList.remove('palette2'); 
+}
+
+function deletePalette3() {
+previewRectangle.classList.remove('palette3');
+borderMobile.classList.remove('palette3'); 
+borderEmail.classList.remove('palette3'); 
+borderLinkedin.classList.remove('palette3'); 
+borderGithub.classList.remove('palette3');
+previewName.classList.remove('palette3');
+ 
+}
+
+function addPalette2() {
+previewRectangle.classList.add('palette2');
+borderMobile.classList.add('palette2'); 
+borderEmail.classList.add('palette2'); 
+borderLinkedin.classList.add('palette2'); 
+borderGithub.classList.add('palette2');
+previewName.classList.add('palette2');
+ 
+}
+
+function addPalette3() {
+previewRectangle.classList.add('palette3');
+borderMobile.classList.add('palette3'); 
+borderEmail.classList.add('palette3'); 
+borderLinkedin.classList.add('palette3'); 
+borderGithub.classList.add('palette3');
+previewName.classList.add('palette3');
+ 
+}
+
+function handleClickPalette1(event) {
+    deletePalette2();
+    deletePalette3();
+    
+}
+
+function handleClickPalette2(event) {
+    deletePalette3();
+    addPalette2();
+    
+}
+
+function handleClickPalette3(event) {
+    deletePalette2();
+    addPalette3();
+    
+}
+
+buttonPalette1.addEventListener('click', handleClickPalette1);
+buttonPalette2.addEventListener('click', handleClickPalette2);
+buttonPalette3.addEventListener('click', handleClickPalette3);
+
+
+// ------------------------RELLENO TARJETA----------
+
+function handleInputName(event) {
+    const nameWritten = inputName.value;
+
+    if (nameWritten === '') {
+    previewName.innerHTML = 'Nombre y Apellido';
+    } else {
+    previewName.innerHTML = nameWritten;
+    }
+}
+
+inputName.addEventListener('input', handleInputName);
+
+
+function handleInputJob(event) {
+    const jobWritten = inputJob.value;
+
+    if (jobWritten === '') {
+    previewJob.innerHTML = 'Front-end developer';
+    } else {
+    previewJob.innerHTML = jobWritten;
+    }
+}
+
+inputJob.addEventListener('input', handleInputJob);
+
+function handleInputMail(event) {
+    const mailWritten = inputMail.value;
+    previewMail.href= `mailto:${mailWritten}`;
+}
+
+inputMail.addEventListener('input', handleInputMail);
+
+function handleInputPhone(event) {
+    const phoneWritten = inputPhone.value;
+    previewPhone.href = `tel:${phoneWritten}`;
+}
+
+inputPhone.addEventListener('input', handleInputPhone);
+
+function handleInputLinkedin(event) {
+    const linkedinWritten = inputLinkedin.value;
+    previewLinkedin.href = linkedinWritten;
+}
+
+inputLinkedin.addEventListener('input', handleInputLinkedin);
+
+function handleInputGithub(event) {
+    const githubWritten = inputGithub.value;
+    previewGithub.href = githubWritten;
+}
+
+inputGithub.addEventListener('input', handleInputGithub);
