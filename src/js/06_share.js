37a15af1f2210c6509d1f698aacfc922ae3bfe00
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-02-22 18:21:37 +0100

    add changes

--- src/js/06_share.js
+++ src/js/06_share.js
@@ -1,22 +1,22 @@
 /* eslint-disable strict */
 function handleClickSend(event) {
-event.preventDefault();
-fetch('https://dev.adalab.es/api/card/', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data)
-})
-.then((response) => response.json() )
-.then((data) => {
-    if(data.success){ 
-        linkCard.innerHTML = data.cardURL;
-        linkCard.href = data.cardURL;
+    event.preventDefault();
+    fetch('https://dev.adalab.es/api/card/', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+        .then((response) => response.json() )
+        .then((data) => {
+            if(data.success){ 
+                linkCard.innerHTML = data.cardURL;
+                linkCard.href = data.cardURL;
+                
+                twitterSection.classList.remove("display--none");
+                sendBtn.classList.add("clicked");
+            }
         
-        twitterSection.classList.remove("display--none");
-        sendBtn.classList.add("clicked");
-    }
- 
-})
+        })
 };
 
 
--- src/js/07_localStorage.js
+++ src/js/07_localStorage.js
@@ -1,17 +1,17 @@
 // console.log(inputs[0].name.value);
 console.log(data)
-if (localStorageData !== undefined) {
+if (localStorageData) {
     data = localStorageData;
     
-    updatePreview()
+    inputName.value = data.name;
+    inputJob.value = data.job;
+    //inputPhoto.value = data.photo;
+    inputEmail.value = data.email;
+    inputPhone.value = data.phone;
+    inputLinkedin.value = data.linkedin;
+    inputGithub.value = data.github;
 
-     inputName.value = data.name;
-     inputJob.value = data.job;
-     //inputPhoto.value = data.photo;
-     inputEmail.value = data.email;
-     inputPhone.value = data.phone;
-     inputLinkedin.value = data.linkedin;
-     inputGithub.value = data.github;
+    updatePreview()
 }
 
 
