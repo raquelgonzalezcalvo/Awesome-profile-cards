e490abb578a710d9b71f328bb963d18303c75bff
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-22 20:00:10 +0100

    add changes in localStorage to view the photo

--- src/js/02_preview.js
+++ src/js/02_preview.js
@@ -3,7 +3,23 @@
 
 // inputName.value = pepe;
 
-const localStorageData = JSON.parse(localStorage.getItem('formData'))
+const localStorageData = JSON.parse(localStorage.getItem('formData'));
+
+// console.log(inputs[0].name.value);
+console.log(data)
+if (localStorageData) {
+    data = localStorageData;
+    
+    updatePreview()
+
+     inputName.value = data.name;
+     inputJob.value = data.job;
+     //inputPhoto.value = data.photo;
+     inputEmail.value = data.email;
+     inputPhone.value = data.phone;
+     inputLinkedin.value = data.linkedin;
+     inputGithub.value = data.github;
+}
 
 function updatePreview() {
     if (data.name === '') {
--- src/js/07_localStorage.js
+++ src/js/07_localStorage.js
@@ -1,17 +1,3 @@
-// console.log(inputs[0].name.value);
-console.log(data)
-if (localStorageData !== undefined) {
-    data = localStorageData;
-    
-    updatePreview()
 
-     inputName.value = data.name;
-     inputJob.value = data.job;
-     //inputPhoto.value = data.photo;
-     inputEmail.value = data.email;
-     inputPhone.value = data.phone;
-     inputLinkedin.value = data.linkedin;
-     inputGithub.value = data.github;
-}
 
 
