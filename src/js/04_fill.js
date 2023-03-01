29599692697c241058aa0ab81ec9eb176618de4a
LaiaRuizM <laia.ruizm@hotmail.com>
2023-03-01 13:54:59 +0100

    add required field to each input - show error msg

--- src/js/04_fill.js
+++ src/js/04_fill.js
@@ -1,13 +1,21 @@
 function showErrorMsg(element) {
-  const smallElement = errorMsg.find(eachErrorMsg => eachErrorMsg.attributes.pepino === element.id);
-  console.log(errorMsg);
-   for (const smallElement of errorMsg) { 
-       smallElement.innerHTML = ""; 
-       smallElement.innerHTML += `<i class="fa-solid fa-circle-exclamation"></i> Introduce tu ${smallElement.id}`;
-    }
-
-  
-         
+  // let smallElement;
+  for (let i = 0; i < errorMsg.length; i++) {
+      console.log(errorMsg);
+   if (errorMsg[i].attributes.error.value === element.id) {
+    errorMsg[i].innerHTML = "";
+    errorMsg[i].innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> Introduce tu ${errorMsg[i].id}`;
+  //  eachErrorMsg.attributes.pepino === element.id;
+  //  smallElement = eachErrorMsg;
+  addClassError()
+  }
+  }
+  // const smallElement = errorMsg.find(eachErrorMsg => eachErrorMsg.attributes.pepino === element.id);
+  // console.log(errorMsg);
+  //  for (const smallElement of errorMsg) { 
+  //      smallElement.innerHTML = ""; 
+  //      smallElement.innerHTML += `<i class="fa-solid fa-circle-exclamation"></i> Introduce tu ${smallElement.id}`;
+  //   }
 }
 
   function addClassError() {
@@ -16,12 +24,11 @@ function showErrorMsg(element) {
     }
   }
 
-
 function handleInputForm(event) {
  data[event.target.name] = event.target.value;
  console.log(data[event.target.name]);
  updatePreview();
- if (event.target.value === ""){    
+ if (!event.target.value){    
     showErrorMsg(event.target);
     console.log(event.target)
     addClassError()
@@ -29,8 +36,6 @@ function handleInputForm(event) {
   localStorage.setItem('formData', JSON.stringify(data))
   console.log(data)
 }
-
-
 
 for (let i = 0; i < allInputsList.length; i++) {
     allInputsList[i].addEventListener('input', handleInputForm);
