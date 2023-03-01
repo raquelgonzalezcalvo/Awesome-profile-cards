42ee7eaecd514b9addac382c96f61e21bc210928
LaiaRuizM <laia.ruizm@hotmail.com>
2023-03-01 16:05:38 +0100

    add inputs message error

--- src/js/04_fill.js
+++ src/js/04_fill.js
@@ -1,40 +1,48 @@
 function showErrorMsg(element) {
-  // let smallElement;
   for (let i = 0; i < errorMsg.length; i++) {
-      console.log(errorMsg);
    if (errorMsg[i].attributes.error.value === element.id) {
     errorMsg[i].innerHTML = "";
     errorMsg[i].innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> Introduce tu ${errorMsg[i].id}`;
-  //  eachErrorMsg.attributes.pepino === element.id;
-  //  smallElement = eachErrorMsg;
-  addClassError()
   }
   }
-  // const smallElement = errorMsg.find(eachErrorMsg => eachErrorMsg.attributes.pepino === element.id);
-  // console.log(errorMsg);
-  //  for (const smallElement of errorMsg) { 
-  //      smallElement.innerHTML = ""; 
-  //      smallElement.innerHTML += `<i class="fa-solid fa-circle-exclamation"></i> Introduce tu ${smallElement.id}`;
-  //   }
 }
 
-  function addClassError() {
-    for (const input of allInputsList) {
-      input.classList.add('js-border-input')
-    }
+function addClassError(element) {
+  for (let i = 0; i < allInputsList.length; i++) {
+   if (allInputsList[i].id === element.id) {
+    allInputsList[i].classList.add('js-border-input');
+    allInputsList[4].classList.remove('js-border-input');
   }
+  }
+}
 
 function handleInputForm(event) {
  data[event.target.name] = event.target.value;
- console.log(data[event.target.name]);
  updatePreview();
  if (!event.target.value){    
     showErrorMsg(event.target);
-    console.log(event.target)
-    addClassError()
+    addClassError(event.target);
+} else {
+  hideShowErrorMsg(event.target);
+  removeClassError(event.target);
 }
   localStorage.setItem('formData', JSON.stringify(data))
-  console.log(data)
+}
+
+function hideShowErrorMsg(element) {
+  for (let i = 0; i < errorMsg.length; i++) {
+   if (errorMsg[i].attributes.error.value === element.id) {
+    errorMsg[i].innerHTML = "";
+  }
+  }
+}
+
+function removeClassError(element) {
+  for (let i = 0; i < allInputsList.length; i++) {
+   if (allInputsList[i].id === element.id) {
+    allInputsList[i].classList.remove('js-border-input');
+  }
+  }
 }
 
 for (let i = 0; i < allInputsList.length; i++) {
