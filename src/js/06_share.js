77a5fa96c7ff9272a7d926a36fa45fcb43e4ccb1
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-02 07:53:25 +0100

    add last changes

--- src/js/06_share.js
+++ src/js/06_share.js
@@ -9,16 +9,17 @@ fetch('https://dev.adalab.es/api/card/', {
 .then((data) => {
     const textError = document.querySelector('.js-dataError');
     const twitterLink = document.querySelector('.js-share-twitter');
+    const sectionTwitter = document.querySelector('.js-twitter-section');
     if(!data.success){
         if (data.error.includes('Database error')){
             textError.innerHTML = `<p class='text-error'>Por favor, sube una imagen menor a 200x200px y 120kb</p>`;
             sectionTwitter.classList.add('display--none');
         }else{
-            textError.innerHTML = `<p class='text-error'>¡Algo estás haciendo mal! Inténtalo de nuevo</p>`;
+            textError.innerHTML = `<p class='text-error'>¡Algo estás haciendo mal! Por favor inténtalo de nuevo.</p>`;
             sectionTwitter.classList.add('display--none');
         }
     } else {
-        const sectionTwitter = document.querySelector('.js-twitter-section');
+        textError.classList.add('display--none')
         sectionTwitter.classList.remove('display--none')
         linkCard.innerHTML = data.cardURL;
         linkCard.href = data.cardURL;
